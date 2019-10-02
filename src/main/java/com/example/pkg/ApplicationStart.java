5c39833bd67388454c10fca1e1931c4b4b6a9704
Masha Budryte <masha.budryte@thoughtworks.com>
2019-10-02 11:02:01 +0100

    Bean for a 3D point added

--- src/main/java/com/example/pkg/ApplicationStart.java
+++ src/main/java/com/example/pkg/ApplicationStart.java
@@ -6,8 +6,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationStart {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Beans.xml");
-        Point p = (Point) applicationContext.getBean("Point10");
-        System.out.println(p.getX());
-        System.out.println(p.getY());
+
+        System.out.println("------------------");
+        System.out.println("Point10:");
+        Point point10 = (Point) applicationContext.getBean("Point10");
+        System.out.println(point10.getX());
+        System.out.println(point10.getY());
+
+        System.out.println("------------------");
+        System.out.println("threeDPoint11:");
+        threeDPoint threeDPoint10 = (threeDPoint) applicationContext.getBean("threeDPoint11");
+        System.out.println(threeDPoint10.getX());
+        System.out.println(threeDPoint10.getY());
+        System.out.println(threeDPoint10.getZ());
+
     }
 }
