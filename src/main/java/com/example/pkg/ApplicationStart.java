fb62d24b9592eb0d1e4072ae61e05c29b6a154f0
Masha Budryte <masha.budryte@thoughtworks.com>
2019-10-02 14:33:54 +0100

    showing the scope difference

--- src/main/java/com/example/pkg/ApplicationStart.java
+++ src/main/java/com/example/pkg/ApplicationStart.java
@@ -7,6 +7,9 @@ public class ApplicationStart {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Beans.xml");
         Animal animal = (Animal) applicationContext.getBean("animal");
+
+        Color color = (Color) applicationContext.getBean("blackAndWhite1");
+        System.out.println(color.getColorId());
         System.out.println(animal.getColor().getColorName());
     }
 }
