4348c62e7059cdc308d66ff861772e8271e1c116
Masha Budryte <masha.budryte@thoughtworks.com>
2019-10-02 13:38:07 +0100

    Created a Calculator bean

--- src/main/java/com/example/pkg/ApplicationStart.java
+++ src/main/java/com/example/pkg/ApplicationStart.java
@@ -6,5 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationStart {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Beans.xml");
+        Calculator calculator = (Calculator) applicationContext.getBean("calc");
+
+        System.out.println(calculator.add());
+        System.out.println(calculator.divide());
+        System.out.println(calculator.multiply());
+        System.out.println(calculator.substract());
     }
 }
