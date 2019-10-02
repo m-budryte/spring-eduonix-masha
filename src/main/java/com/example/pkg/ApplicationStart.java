ec2d0e898a5378cd12645029a588b4bd1dfe77bb
Masha Budryte <masha.budryte@thoughtworks.com>
2019-10-02 16:55:25 +0100

    autowire by constructor, name and type practice

--- src/main/java/com/example/pkg/ApplicationStart.java
+++ src/main/java/com/example/pkg/ApplicationStart.java
@@ -6,8 +6,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationStart {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Beans.xml");
-        Animal zebra = (Animal) applicationContext.getBean("zebra1");
+        Animal zebra1 = (Animal) applicationContext.getBean("zebra1");
+        Animal zebra2 = (Animal) applicationContext.getBean("zebra2");
+        Animal zebra3 = (Animal) applicationContext.getBean("zebra3");
 
-        System.out.println(zebra.getColor().getColorName());
+        System.out.println(zebra1.getColor().getColorName());
+        System.out.println(zebra2.getColor().getColorName());
+        System.out.println(zebra3.getColor().getColorName());
     }
 }
